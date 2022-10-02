9c1bb4ed63353be6c5e3fae69f4b4f0c83049de6
AK <alexanderkey0508@gmail.com>
2022-10-02 15:22:54 +0200

    find ball location

--- GoChaseIt/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ GoChaseIt/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -1,6 +1,9 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
+#include <iostream>
+
+using namespace std;
 
 // Define a global client that can request services
 ros::ServiceClient client;
@@ -14,25 +17,43 @@ void drive_robot(float lin_x, float ang_z)
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-    ROS_INFO_STREAM("image");
+    // ROS_INFO_STREAM("image");
     int white_pixel = 255;
+    bool ball_found = false;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
 
-    // Loop through each pixel in the image and check if its equal to the first one
-    for (int i = 0; i < img.height * img.step; i++) {
-        ROS_INFO_STREAM(img.data[i]);
-        if (img.data[i] == white_pixel){
-            ROS_INFO_STREAM("Ball!");
-        }
+    int i = 0; 
+    while (!ball_found && i < img.height * img.step){
+
+        // ROS_INFO_STREAM(img.data[i]);
+            // cout << img.data[i];
+
+            if (img.data[i] == white_pixel)
+            {
+                int y_position = i % 800;
+                if (y_position < 800 / 3)
+                    cout << ("left\n");
+                else if (y_position >= 800 / 3 && y_position < 800 / 3 * 2)
+                    cout << ("center\n");
+                else if (y_position >= 800 / 3 * 2 && y_position < 800)
+                    cout << ("right\n");
+
+                ball_found = true;
+                // ROS_INFO_STREAM("Ball!");
+            }
         
+        i++; 
     }
+
+    if (!ball_found) cout << "no ball\n";
+    
 }
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     // Initialize the process_image node and create a handle to it
     ros::init(argc, argv, "process_image");
