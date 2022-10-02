c4dd2074b1bf286fb11974494f82f2ef03c44e0a
AK <alexanderkey0508@gmail.com>
2022-10-02 17:21:20 +0200

    get position and chase

--- GoChaseIt/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ GoChaseIt/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "ball_chaser/DriveToTarget.h"
 #include <sensor_msgs/Image.h>
+#include "geometry_msgs/Twist.h"
 #include <iostream>
 
 using namespace std;
@@ -9,10 +10,21 @@ using namespace std;
 ros::ServiceClient client;
 
 // This function calls the command_robot service to drive the robot in the specified direction
-void drive_robot(float lin_x, float ang_z)
+void drive_robot(float angular_z, float linear_x)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    ROS_INFO("Request to move received");
+
+    ball_chaser::DriveToTarget srv; 
+    srv.request.angular_z = angular_z; 
+    
+    // TODO set this
+    srv.request.linear_x = linear_x; 
+    
+    if (!client.call(srv))
+        ROS_ERROR("Failed to call service command_robot");
+ 
 }
+
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
@@ -20,6 +32,7 @@ void process_image_callback(const sensor_msgs::Image img)
     // ROS_INFO_STREAM("image");
     int white_pixel = 255;
     bool ball_found = false;
+    float angular_z = 0;
 
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
@@ -34,13 +47,23 @@ void process_image_callback(const sensor_msgs::Image img)
 
             if (img.data[i] == white_pixel)
             {
-                int y_position = i % 800;
-                if (y_position < 800 / 3)
-                    cout << ("left\n");
-                else if (y_position >= 800 / 3 && y_position < 800 / 3 * 2)
-                    cout << ("center\n");
-                else if (y_position >= 800 / 3 * 2 && y_position < 800)
-                    cout << ("right\n");
+                int y_position = i % img.step;
+
+                // ball is to the left (so we turn left)
+                if (y_position < img.step / 3)
+                    angular_z = 1; 
+
+                // ball is at the center
+                else if (y_position >= img.step / 3 && y_position < img.step / 3 * 2)
+                    angular_z = 0;
+                
+                // ball is to the right
+                else if (y_position >= img.step / 3 * 2 && y_position < img.step)
+                    angular_z = -1; 
+
+                cout << ("angular_z is ") << angular_z << "\n"; 
+
+                drive_robot(angular_z, 0.2); 
 
                 ball_found = true;
                 // ROS_INFO_STREAM("Ball!");
@@ -49,8 +72,12 @@ void process_image_callback(const sensor_msgs::Image img)
         i++; 
     }
 
-    if (!ball_found) cout << "no ball\n";
-    
+    if (!ball_found) {
+        
+        // stop the robot
+        drive_robot(0,0); 
+        cout << "no ball\n";
+    }
 }
 
 int main(int argc, char **argv)
